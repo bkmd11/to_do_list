d1f00b4ae63a2566db3ad256f121251c1750ebe6
bkmd11 <bkendall11@gmail.com>
2022-12-25 15:25:08 -0500

    Pretending to be a programer

---
+++ strings.py
@@ -0,0 +1,18 @@
+
+
+OPTIONS = '\nOptions:\n(m) make new list\n(a) add an item\n(e) edit the list\n(d) delete item\n(q) quit\n'
+LAST_WEEK_STRING = 'THE LAST FUCKING WEEK OF THIS SHITTY SHIT!!!'
+DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+DAY_ERROR_MESSAGE = 'That is not a valid day, please re-input assignment\n'
+INT_ERROR = 'please input valid number\n'
+COURSE_LENGTH = '7'
+CLS = 'cls'
+WHEN_DUE = 'When is this due?\n'
+BUILD_MESSAGE = 'What assignments are due this week?\nEnter nothing to quit\n'
+UPDATE_ASSIGNMENT = 'Enter the number of the assignment to update, or leave blank to exit:\n '
+DELETE_MESSAGE = 'Enter the number of the assignment to delete: '
+ASSIGNMENT_MESSAGE = 'ASSIGNMENTS DUE FOR'
+TITLE = 'TODO LIST'
+INPUT_MESSAGE = 'What week is this?\n'
+WEEK = 'WEEK'
+OUTPUT_FILE = 'main_list.json'
--- toDoList.py
+++ toDoList.py
@@ -12,28 +12,8 @@ import time
 
 from colorama import init, Fore, Style
 
+import strings
 
-########################################################################################################################
-# Global variables because I suck
-########################################################################################################################
-
-
-OPTIONS = '\nOptions:\n(m) make new list\n(a) add an item\n(e) edit the list\n(d) delete item\n(q) quit\n'
-LAST_WEEK_STRING = 'THE LAST FUCKING WEEK OF THIS SHITTY SHIT!!!'
-DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-DAY_ERROR_MESSAGE = 'That is not a valid day, please re-input assignment\n'
-INT_ERROR = 'please input valid number\n'
-COURSE_LENGTH = '7'
-CLS = 'cls'
-WHEN_DUE = 'When is this due?\n'
-BUILD_MESSAGE = 'What assignments are due this week?\nEnter nothing to quit\n'
-UPDATE_ASSIGNMENT = 'Enter the number of the assignment to update, or leave blank to exit:\n '
-DELETE_MESSAGE = 'Enter the number of the assignment to delete: '
-ASSIGNMENT_MESSAGE = 'ASSIGNMENTS DUE FOR'
-TITLE = 'TODO LIST'
-INPUT_MESSAGE = 'What week is this?\n'
-WEEK = 'WEEK'
-OUTPUT_FILE = 'main_list.json'
 
 ########################################################################################################################
 # This portion focuses on making the list
@@ -42,13 +22,13 @@ OUTPUT_FILE = 'main_list.json'
 
 def input_week():
     """Takes user input for what week of the course it is"""
-    week_number = input(INPUT_MESSAGE)
+    week_number = input(strings.INPUT_MESSAGE)
 
-    if week_number == COURSE_LENGTH:
-        return LAST_WEEK_STRING
+    if week_number == strings.COURSE_LENGTH:
+        return strings.LAST_WEEK_STRING
 
     else:
-        return f'{WEEK} {week_number}'
+        return f'{strings.WEEK} {week_number}'
 
 
 def input_assignments():
@@ -56,15 +36,15 @@ def input_assignments():
 
     assignment_dictionary = {}
     while True:
-        assignments_to_do = input(BUILD_MESSAGE)
+        assignments_to_do = input(strings.BUILD_MESSAGE)
         if assignments_to_do == '':
             break
-        due_date = input(WHEN_DUE).capitalize()
+        due_date = input(strings.WHEN_DUE).capitalize()
 
-        if due_date in DAYS:
+        if due_date in strings.DAYS:
             assignment_dictionary[assignments_to_do] = [due_date, False]
         else:
-            print(DAY_ERROR_MESSAGE)
+            print(strings.DAY_ERROR_MESSAGE)
 
     return assignment_dictionary
 
@@ -84,7 +64,7 @@ def build():
 def text_display(assignment_dict, week_number):
     """This is the display that shows what is due this week"""
     sorted_dict = dict(sorted(assignment_dict.items(), key=day_index))
-    todo_list = f'{ASSIGNMENT_MESSAGE} {week_number}'
+    todo_list = f'{strings.ASSIGNMENT_MESSAGE} {week_number}'
     count = 1
     for assignment, due_date in sorted_dict.items():
         if due_date[1] is False:
@@ -98,7 +78,7 @@ def text_display(assignment_dict, week_number):
 
 def day_index(day):
     """I return an index for the day list so the text display is in order"""
-    index = DAYS.index(day[1][0].capitalize())
+    index = strings.DAYS.index(day[1][0].capitalize())
 
     return index
 ########################################################################################################################
@@ -118,15 +98,15 @@ def show_assignments(assignment_dict):
             else:
                 print(f'{Fore.RED}{items.index(i) + 1}: {i}{Fore.RESET}')
 
-        index = input(UPDATE_ASSIGNMENT)
+        index = input(strings.UPDATE_ASSIGNMENT)
         if index == '':
             break
         try:
             return items[int(index) - 1]
         except IndexError as e:
-            print(f'{e} {INT_ERROR}')
+            print(f'{e} {strings.INT_ERROR}')
         except ValueError as e:
-            print(f'{e} {INT_ERROR}')
+            print(f'{e} {strings.INT_ERROR}')
 
 
 def change_state(due_date_bool):
@@ -141,16 +121,16 @@ def change_state(due_date_bool):
 def append_items():
     """Adds items to the list"""
     while True:
-        assignments_to_do = input(BUILD_MESSAGE)
+        assignments_to_do = input(strings.BUILD_MESSAGE)
         if assignments_to_do == '':
             break
-        due_date = input(WHEN_DUE).capitalize()
+        due_date = input(strings.WHEN_DUE).capitalize()
 
-        if due_date in DAYS:
+        if due_date in strings.DAYS:
             return assignments_to_do, [due_date, False]
 
         else:
-            print(DAY_ERROR_MESSAGE)
+            print(strings.DAY_ERROR_MESSAGE)
 
 
 def check_complete(assignments_due):
@@ -168,7 +148,7 @@ def delete_item(assignment_dict):
     for i in items:
         print(f'{items.index(i) + 1}: {i}')
 
-    index = int(input(DELETE_MESSAGE))
+    index = int(input(strings.DELETE_MESSAGE))
 
     assignment_dict.pop(items[index - 1])
 
@@ -178,7 +158,7 @@ def delete_item(assignment_dict):
 ########################################################################################################################
 def finished(week):
     """I take a week and figure out if it is the last week, then return a function"""
-    if week == LAST_WEEK_STRING:
+    if week == strings.LAST_WEEK_STRING:
         finished_class()
     else:
         finished_week()
@@ -186,7 +166,7 @@ def finished(week):
 
 def finished_week():
     """ Prints a fun message when I finish all assignments"""
-    os.system(CLS)
+    os.system(strings.CLS)
     colors = list(vars(Fore).values())
     count = 0
     while count != 11:
@@ -205,7 +185,7 @@ def finished_week():
 def finished_class():
     # Todo: add credit countdown
     """ Prints a fun message when I finish a class"""
-    os.system(CLS)
+    os.system(strings.CLS)
     colors = list(vars(Fore).values())
     count = 0
     while count != 11:
@@ -229,12 +209,12 @@ def finished_class():
 def main(week_number, assignments_due):
     init()
     while True:
-        os.system(CLS)
+        os.system(strings.CLS)
         to_do_list = text_display(assignments_due, week_number)
-        print(f'{Fore.CYAN}{TITLE}{Fore.RESET}')
+        print(f'{Fore.CYAN}{strings.TITLE}{Fore.RESET}')
         print(to_do_list)
-        option = input(OPTIONS)
-        os.system(CLS)
+        option = input(strings.OPTIONS)
+        os.system(strings.CLS)
         if option.lower() == 'm':
             week_number, assignments_due = build()
 
@@ -273,7 +253,7 @@ def main(week_number, assignments_due):
 
 if __name__ == '__main__':
     try:
-        with open(OUTPUT_FILE, 'r') as file:
+        with open(strings.OUTPUT_FILE, 'r') as file:
             json_data = json.load(file)
 
         week_num = json_data[0]
@@ -286,5 +266,5 @@ if __name__ == '__main__':
     week_num, assignments = main(week_num, assignments)
 
     json_data = [week_num, assignments]
-    with open(OUTPUT_FILE, 'w') as file:
+    with open(strings.OUTPUT_FILE, 'w') as file:
         json.dump(json_data, file, indent=4)
