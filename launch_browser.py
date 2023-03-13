d04d579b6bbbfe2a6bfc1f4ed4ad84d55056b70d
bkmd11 <bkendall11@gmail.com>
2023-03-13 09:00:12 -0400

    made launch feature

---
+++ launch_browser.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+import strings
+
+
+def start_class():
+    webbrowser.open_new_tab(strings.GMAIL)
+    webbrowser.open_new_tab(strings.DRIVE)
+    webbrowser.open(strings.DASHBOARD)
+
