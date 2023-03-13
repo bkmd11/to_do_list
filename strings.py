79da200c2d27eeb4179c1d6ac8442f3a4d33a75d
bkmd11 <bkendall11@gmail.com>
2023-03-13 09:00:56 -0400

    edited strings

--- strings.py
+++ strings.py
@@ -1,6 +1,14 @@
 """I am the global strings"""
+# TODO organize these
 
-OPTIONS = '\nOptions:\n(m) make new list\n(a) add an item\n(e) edit the list\n(d) delete item\n(q) quit\n'
+OPTIONS = '''\nOptions:
+            \n(m) make new list
+            \n(a) add an item
+            \n(e) edit the list
+            \n(d) delete item
+            \n(l) launch browser
+            \n(q) quit
+            \n'''
 LAST_WEEK_STRING = 'THE LAST FUCKING WEEK OF THIS SHITTY SHIT!!!'
 DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 DAY_ERROR_MESSAGE = 'That is not a valid day, please re-input assignment\n'
@@ -9,10 +17,14 @@ COURSE_LENGTH = '7'
 CLS = 'cls'
 WHEN_DUE = 'When is this due?\n'
 BUILD_MESSAGE = 'What assignments are due this week?\nEnter nothing to quit\n'
-UPDATE_ASSIGNMENT = 'Enter the number of the assignment to update, or leave blank to exit:\n '
+UPDATE_ASSIGNMENT = 'Enter the number of the assignment to update, or leave blank to exit:\n'
 DELETE_MESSAGE = 'Enter the number of the assignment to delete: '
 ASSIGNMENT_MESSAGE = 'ASSIGNMENTS DUE FOR'
 TITLE = 'TODO LIST'
 INPUT_MESSAGE = 'What week is this?\n'
 WEEK = 'WEEK'
 OUTPUT_FILE = 'main_list.json'
+
+DASHBOARD = 'https://champlain.instructure.com/'
+GMAIL = 'https://mail.google.com/mail/u/0/#inbox'
+DRIVE = 'https://drive.google.com/drive/my-drive'
