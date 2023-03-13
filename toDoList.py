b65057a7411395a73343d67ee09bbeb1adc3d6aa
bkmd11 <bkendall11@gmail.com>
2023-03-13 08:39:44 -0400

    added todo

--- toDoList.py
+++ toDoList.py
@@ -3,6 +3,8 @@ Using this to plan my week for classes.
 Todo:
     Error handling -- if anything is corrupt in json file, it breaks
     sort by done at top -- this will require a change in data structure
+    could modulate more with main.py and a sub directory
+    add launch web browser feature
 """
 import json
 import os
