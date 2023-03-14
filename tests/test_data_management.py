8392759877e6de103215798e51e9ce0ef2c0a45d
bkmd11 <bkendall11@gmail.com>
2023-03-14 15:49:22 -0400

    my shitty code is haunting me

--- tests/test_data_management.py
+++ tests/test_data_management.py
@@ -9,3 +9,11 @@ class TestChangeState(unittest.TestCase):
 
     def test_false_to_true(self):
         self.assertFalse(data_management.change_state(True))
+
+
+class TestCheckComplete(unittest.TestCase):
+    def test_assignments_done(self):
+        self.assertTrue(data_management.check_complete({'spam': ['mon', True], 'eggs': ['tue', True]}))
+
+    def test_assignments_not_done(self):
+        self.assertFalse(data_management.check_complete({'spam': ['mon', True], 'eggs': ['tue', False]}))
